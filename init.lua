c7809bb29bd5a02d7d8e8014fb27ccd736f2c37b
Walter Fabián Rodríguez Salazar <wfabianrs@gmail.com>
2023-11-21 11:55:22 -0500

    :tada:  Commit inicial, configuración base
    El código está basado en la serie de tutoriales de alpha2phi
    
    https://alpha2phi.medium.com/learn-neovim-the-practical-way-8818fcf4830f
    
    Sección "Learning Series — Neovim PDE"

---
+++ init.lua
@@ -0,0 +1,17 @@
+require("config.options")
+require("config.lazy")
+
+print("nvim loaded arg " .. vim.fn.argc(-1))
+if vim.fn.argc(-1) == 0 then
+	vim.api.nvim_create_autocmd("User", {
+		group = vim.api.nvim_create_augroup("NeovimPDE", { clear = true }),
+		pattern = "VeryLazy",
+		callback = function()
+			require("config.autocmds")
+			require("config.keymaps")
+		end,
+	})
+else
+	require("config.autocmds")
+	require("config.keymaps")
+end
