0178e2a85808ddb50caf53d9dd437afa3b3abd3c
Walter Fabián Rodríguez Salazar <wfabianrs@gmail.com>
2023-11-21 16:36:32 -0500

    :construction: (keymap) Configuración de keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,11 @@
 local keymap = vim.keymap.set
 
+-- General
+keymap("n", "<leader>ww", ":w!<CR>")
+keymap("n", "<leader>wa", ":wall!<CR>")
+keymap("n", "<leader>qq", ":q!<CR>")
+keymap("n", "<leader>qa", ":qall!<CR>")
+
 -- Remap for dealing with word wrap
 keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
 keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
@@ -18,13 +24,43 @@ keymap("n", "<C-u>", "<C-u>zz")
 keymap("n", "]p", "o<Esc>p", { desc = "Paste below" })
 keymap("n", "]P", "O<Esc>p", { desc = "Paste above" })
 
--- Better escape using jk in insert and terminal mode
-keymap("i", "jk", "<ESC>")
-keymap("t", "jk", "<C-\\><C-n>")
-keymap("t", "<C-h>", "<C-\\><C-n><C-w>h")
-keymap("t", "<C-j>", "<C-\\><C-n><C-w>j")
-keymap("t", "<C-k>", "<C-\\><C-n><C-w>k")
-keymap("t", "<C-l>", "<C-\\><C-n><C-w>l")
+-- Better escape
+keymap("n", "<Esc>", ":nohl<CR>:echo<CR>") -- ESC in normal mode)
+keymap("i", "<M-f>", "<ESC>")
+keymap("t", "<M-f>", "<C-\\><C-n>")
+
+-- Movements Dvorak
+-- Normal Mode
+keymap("n", "<M-r>", "h")
+keymap("n", "<M-t>", "j")
+keymap("n", "<M-n>", "k")
+keymap("n", "<M-s>", "l")
+-- Visual Mode
+keymap("x", "<M-r>", "h")
+keymap("x", "<M-t>", "j")
+keymap("x", "<M-n>", "k")
+keymap("x", "<M-s>", "l")
+-- Insert Mode
+keymap("i", "<M-r>", "<Left>")
+keymap("i", "<M-t>", "<Down>")
+keymap("i", "<M-n>", "<Up>")
+keymap("i", "<M-s>", "<Right>")
+-- Terminol mode
+keymap("t", "<C-r>", "<C-\\><C-n><C-w>h")
+keymap("t", "<C-t>", "<C-\\><C-n><C-w>j")
+keymap("t", "<C-n>", "<C-\\><C-n><C-w>k")
+keymap("t", "<C-s>", "<C-\\><C-n><C-w>l")
+-- Movements between buffers
+keymap("n", "<C-r>", "<C-w>h")
+keymap("n", "<C-t>", "<C-w>j")
+keymap("n", "<C-n>", "<C-w>k")
+keymap("n", "<C-s>", "<C-w>l")
+
+-- Buffer
+keymap("n", "<C-h>", ":bp<CR>")
+keymap("n", "<C-l>", ":bn<CR>")
+keymap("n", "<leader>bd", ":bd<CR>")
+keymap("n", "<leader>bdd", ":bd!<CR>")
 
 -- Add undo break-points
 keymap("i", ",", ",<c-g>u")
@@ -37,6 +73,10 @@ keymap("v", ">", ">gv")
 
 -- Paste over currently selected text without yanking it
 keymap("v", "p", '"_dp')
+-- X clipboard
+keymap("x", "<leader>y", '"+y')
+keymap("n", "<leader>p", '"+p')
+keymap("n", "<leader>P", '"+P')
 
 -- Insert blank line
 keymap("n", "]<Space>", "o<Esc>")
@@ -44,9 +84,9 @@ keymap("n", "[<Space>", "O<Esc>")
 
 -- Auto indent
 keymap("n", "i", function()
-	if #vim.fn.getline(".") == 0 then
-		return [["_cc]]
-	else
-		return "i"
-	end
+  if #vim.fn.getline "." == 0 then
+    return [["_cc]]
+  else
+    return "i"
+  end
 end, { expr = true })
