d2ae88a24c26adc9e32234ae3bf829f09ec2677c
Walter Fabián Rodríguez Salazar <wfabianrs@gmail.com>
2023-11-21 16:28:24 -0500

    :construction: (autocmd) Creados autocomandos para cambio de números

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,56 +1,97 @@
 local function augroup(name)
-	return vim.api.nvim_create_augroup("nde_" .. name, { clear = true })
+  return vim.api.nvim_create_augroup("nde_" .. name, { clear = true })
 end
 
 -- See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-	group = augroup("highlight_yank"),
-	pattern = "*",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = augroup "highlight_yank",
+  pattern = "*",
 })
 
 -- Go to last loction when opening a buffer
 vim.api.nvim_create_autocmd("BufReadPost", {
-	group = augroup("last_loc"),
-	callback = function()
-		local mark = vim.api.nvim_buf_get_mark(0, '"')
-		local lcount = vim.api.nvim_buf_line_count(0)
-		if mark[1] > 0 and mark[1] <= lcount then
-			pcall(vim.api.nvim_win_set_cursor, 0, mark)
-		end
-	end,
+  group = augroup "last_loc",
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
 })
 
 -- windows to close
 vim.api.nvim_create_autocmd("FileType", {
-	group = augroup("close_with_q"),
-	pattern = {
-		"OverseerForm",
-		"OverseerList",
-		"checkhealth",
-		"floggraph",
-		"fugitive",
-		"git",
-		"gitcommit",
-		"help",
-		"lspinfo",
-		"man",
-		"neotest-output",
-		"neotest-summary",
-		"qf",
-		"query",
-		"spectre_panel",
-		"startuptime",
-		"toggleterm",
-		"tsplayground",
-		"vim",
-		"neoai-input",
-		"neoai-output",
-	},
-	callback = function(event)
-		vim.bo[event.buf].buflisted = false
-		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
-	end,
+  group = augroup "close_with_q",
+  pattern = {
+    "OverseerForm",
+    "OverseerList",
+    "checkhealth",
+    "floggraph",
+    "fugitive",
+    "git",
+    "gitcommit",
+    "help",
+    "lspinfo",
+    "man",
+    "neotest-output",
+    "neotest-summary",
+    "qf",
+    "query",
+    "spectre_panel",
+    "startuptime",
+    "toggleterm",
+    "tsplayground",
+    "vim",
+    "neoai-input",
+    "neoai-output",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
 })
+
+-- Alternar automáticamente entre números de línea relativos y absolutos
+-- Los números relativos se utilizan en un búfer que tiene el foco y está en modo normal, ya que es ahí donde te
+-- mueves.
+-- Se desactivan cuando sales de Vim, cambias a otra división o entra en los modos de inserción y comando.
+local augroup = vim.api.nvim_create_augroup("numbertoggle", {})
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+  pattern = "*",
+  group = augroup,
+  callback = function()
+    if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
+      vim.opt.relativenumber = true
+    end
+  end,
+})
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+  pattern = "*",
+  group = augroup,
+  callback = function()
+    if vim.o.nu then
+      vim.opt.relativenumber = false
+      vim.cmd "redraw"
+    end
+  end,
+})
+
+local lastEdited = vim.api.nvim_create_augroup("lastEdited", {})
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*",
+  group = lastEdited,
+  callback = function()
+    pos = vim.fn.line "."
+    fin = vim.fn.line "$"
+    if pos > 0 and pos <= fin then
+      vim.cmd [[normal! g`"]]
+    end
+  end,
+})
+
+-- don't auto commenting new lines
+cmd [[au BufEnter * set fo-=c fo-=r fo-=o]]
