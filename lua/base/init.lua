3310f8f11efbf38e345e4419258b3932cb6e557e
Walter Fabián Rodríguez Salazar <wfabianrs@gmail.com>
2023-11-21 16:20:16 -0500

    :construction: (code) Incluido consultas a cheat.sh y stackoverflow

--- lua/base/init.lua
+++ lua/base/init.lua
@@ -1,36 +1,52 @@
 return {
-	{ "tpope/vim-sleuth", event = "VeryLazy" },
-	{ "tpope/vim-repeat", event = "VeryLazy" },
-	{ "nvim-lua/plenary.nvim" },
-	{
-		"tpope/vim-fugitive",
-		cmd = { "Git", "GBrowse", "Gdiffsplit", "Gvdiffsplit" },
-		dependencies = {
-			"tpope/vim-rhubarb",
-		},
+  { "tpope/vim-sleuth", event = "VeryLazy" },
+  { "tpope/vim-repeat", event = "VeryLazy" },
+  { "nvim-lua/plenary.nvim" },
+  {
+    "tpope/vim-fugitive",
+    cmd = { "Git", "GBrowse", "Gdiffsplit", "Gvdiffsplit" },
+    dependencies = {
+      "tpope/vim-rhubarb",
+    },
     -- stylua: ignore
     keys = {
       { "<leader>gs", "<cmd>Git<cr>", desc = "Status" },
     },
-	},
-	{
-		"folke/persistence.nvim",
-		event = "BufReadPre",
-		opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help" } },
+  },
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help" } },
     -- stylua: ignore
     keys = {
       { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
       { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
       { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
     },
-	},
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		opts = {
-			defaults = {
-				["<leader>g"] = { name = "+Git" },
-			},
-		},
-	},
+  },
+  { "akinsho/toggleterm.nvim", event = "VeryLazy", config = true },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      defaults = {
+        ["<leader>g"] = { name = "+Git" },
+        ["<leader>s"] = {
+          name = "+Search",
+          c = {
+            function()
+              require("utils.cht").cht()
+            end,
+            "Cheatsheets",
+          },
+          s = {
+            function()
+              require("utils.cht").stack_overflow()
+            end,
+            "Stack Overflow",
+          },
+        },
+      },
+    },
+  },
 }
