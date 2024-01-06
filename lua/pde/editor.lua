3820eaa7fe631364b69f4f606ea23ce586a72b08
Walter Fabian Rodriguez Salazar <wfabianrs@gmail.com>
2024-01-06 01:47:53 -0500

    :construction: [add] Configuración de las herramientas de edición
    Agregado configuración de los plugins:
    
    - Pocco81/true-zen.nvim
    
    - chentoast/marks.nvim
    
    - L3MON4D3/LuaSnip y creación de carpeta de snippets personales
    
    - tpope/vim-fugitive
    
    - lewis6991/gitsigns.nvim

---
+++ lua/pde/editor.lua
@@ -0,0 +1,59 @@
+return {
+    {
+        "Pocco81/true-zen.nvim",
+        cmd = { 'TZNarrow', 'TZFocus', 'TZMinimalist', 'TZAtaraxis' },
+        config = function()
+            local keymap = vim.keymap.set
+            keymap("n", "<leader>zn", ":TZNarrow<CR>", { desc = 'Narrow region' })
+            keymap("v", "<leader>zn", ":'<,'>TZNarrow<CR>", { desc = 'VNarrow region' })
+            keymap("n", "<leader>zf", ":TZFocus<CR>", { desc = 'Focus buffer' })
+            keymap("n", "<leader>zm", ":TZMinimalist<CR>", { desc = 'Min mode' })
+            keymap("n", "<leader>za", ":TZAtaraxis<CR>", { desc = 'Old zen mode' })
+        end,
+    },
+    {
+        "chentoast/marks.nvim",
+        lazy = false,
+        config = function()
+          require("marks").setup()
+        end,
+    },
+    {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*", 
+        lazy = false,
+        config = function()
+            local ls = require("luasnip")
+            local keymap = vim.keymap.set
+            keymap({"i"}, "<C-c>", function()
+                ls.expand()
+            end, {silent = true})
+            keymap({"i", "s"}, "<C-x>", function()
+                ls.jump(1)
+            end, {silent = true})
+            keymap({"i", "s"}, "<C-k>", function()
+                ls.jump(-1)
+            end, {silent = true})
+ 
+            keymap({"i", "s"}, "<C-q>", function()
+                if ls.choice_active() then
+                    ls.change_choice(1)
+                end
+            end, {silent = true})
+            -- ~/.config/nvim/snippets
+            require("luasnip.loaders.from_vscode").load({ paths = { "~/.config/nvim/snippets/" } })
+        end,
+    },
+    {
+        "tpope/vim-fugitive",
+        cmd = { 'G', 'Git' },
+        keys = {
+            { '<leader>ga', '<cmd>Git fetch --all --p<CR>', desc = 'Git fetch' },
+            { '<leader>gp', '<cmd>Git pull<CR>', desc = 'Git pull' },
+        }
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = true,
+    },
+}
