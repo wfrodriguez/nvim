9c890d01e2dc121374435a3898a0ad40350f203e
Walter Fabian Rodriguez Salazar <wfabianrs@gmail.com>
2024-01-05 21:18:25 -0500

    :construction: [add] Configuración de plugins visuales básicos

---
+++ lua/pde/init.lua
@@ -0,0 +1,37 @@
+return {
+    {
+      "gbprod/nord.nvim",
+      lazy = false,
+      priority = 1000,
+      config = function()
+        require("nord").setup {}
+        vim.cmd.colorscheme "nord"
+      end,
+      install = {
+        colorscheme = { "nord" },
+      },
+    },
+    {
+      "nvim-tree/nvim-web-devicons",
+      dependencies = { "DaikyXendo/nvim-material-icon" },
+      config = function()
+        require("nvim-web-devicons").setup {
+          override = require("nvim-material-icon").get_icons(),
+        }
+      end,
+    },
+    {
+      "rcarriga/nvim-notify",
+      lazy = false,
+      priority = 1000,
+      config = function()
+        vim.notify = require "notify"
+        vim.notify.setup()
+      end,
+    },
+    {
+      "MunifTanjim/nui.nvim",
+      config = function()
+      end
+    }
+  }
