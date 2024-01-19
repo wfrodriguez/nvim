db1dd43cbf07f44b9f91df9e427bc0c0a922ac59
Walter Fabián Rodríguez Salazar <wfabianrs@gmail.com>
2024-01-19 18:04:28 -0500

    :technologist: [upd] Cambio de colorscheme

--- lua/pde/init.lua
+++ lua/pde/init.lua
@@ -1,43 +1,43 @@
 return {
-    {
-      "gbprod/nord.nvim",
-      lazy = false,
-      priority = 1000,
-      config = function()
-        require("nord").setup {}
-        vim.cmd.colorscheme "nord"
-      end,
-      install = {
-        colorscheme = { "nord" },
-      },
-    },
-    {
-      "nvim-tree/nvim-web-devicons",
-      dependencies = { "DaikyXendo/nvim-material-icon" },
-      config = function()
-        require("nvim-web-devicons").setup {
-          override = require("nvim-material-icon").get_icons(),
-        }
-      end,
-    },
-    {
-      "rcarriga/nvim-notify",
-      lazy = false,
-      priority = 1000,
-      config = function()
-        vim.notify = require "notify"
-        vim.notify.setup()
-      end,
-    },
-    {
-      "MunifTanjim/nui.nvim",
-      config = function()
-      end
-    },
-    {
-      "SmiteshP/nvim-navic",
-      dependencies = { "neovim/nvim-lspconfig" },
-      lazy = false,
-      priority = 1000,
-    }
-  }
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.o.background = "dark"
+			require("gruvbox").setup({})
+			vim.cmd.colorscheme("gruvbox")
+		end,
+		install = {
+			colorscheme = { "gruvbox" },
+		},
+	},
+	{
+		"nvim-tree/nvim-web-devicons",
+		dependencies = { "DaikyXendo/nvim-material-icon" },
+		config = function()
+			require("nvim-web-devicons").setup({
+				override = require("nvim-material-icon").get_icons(),
+			})
+		end,
+	},
+	{
+		"rcarriga/nvim-notify",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.notify = require("notify")
+			vim.notify.setup()
+		end,
+	},
+	{
+		"MunifTanjim/nui.nvim",
+		config = function() end,
+	},
+	{
+		"SmiteshP/nvim-navic",
+		dependencies = { "neovim/nvim-lspconfig" },
+		lazy = false,
+		priority = 1000,
+	},
+}
